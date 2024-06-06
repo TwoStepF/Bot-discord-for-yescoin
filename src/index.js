cb490025e46b6635f5e40072b1cde1fb40a1b99d
khanhTQ <tranquangkhanh12a2@gmail.com>
2024-06-07 01:57:25 +0700

    create discord bot event

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-// Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+});
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// Log in to Discord with your client's token
+client.on(Events.MessageCreate, async interaction => {
+	if(interaction.author.bot) return;
+	test(interaction);
+});
+
+const test = async (interaction) => {
+	const embed = new EmbedBuilder()
+					.setTitle('Yescoin log info')
+					.setColor(0x18e1ee)
+					.addFields(
+						{ name: '', value: 'Some value here', inline: true },
+						{ name: 'Inline field title', value: 'Some value here', inline: true },
+						{ name: 'Inline field title', value: 'Some value here', inline: true },
+						{ name: '\u200B', value: '\u200B' },
+					).setTimestamp(Date.now())
+		await interaction.reply({ embeds: [embed] });
+}
+
 client.login(process.env.TOKEN);
